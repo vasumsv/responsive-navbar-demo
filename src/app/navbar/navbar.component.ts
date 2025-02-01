ead188d8b71263517a35a79a9aff650e5950bf81
vasumsv <quickproject1312@gmail.com>
2025-02-01 14:48:05 +0530

    first commit

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+  isMenuOpen = false;
+
+  toggleNavbar() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
